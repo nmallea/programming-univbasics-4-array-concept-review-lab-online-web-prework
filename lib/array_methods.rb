01217686e1531910ed432ddba54abc4760811a80
Nikane Mallea <nikane@mac.com>
2019-11-18 07:38:06 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def find_element_index(|orange|)
+ index = orange 
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value([12,13,11,14,9])
+ find_max_value => 14
 end
 
-def find_min_value(array)
-  # Add your solution here
+def find_min_value([2,3,11,4,9])
+  find_min_value => 2
 end
